4086d9ed86bb5c6e0e3181b30ac91866a491c119
Kushagra <kseth0808@gmail.com>
2023-11-08 12:23:28 +0530

    h

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user
+        const user = userCredential.user;
         console.log(user)
         navigate("/")
       })
